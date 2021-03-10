2289f3dd95ac7124e74267c0e62795ca0d872676
Nicolas Gonzalez <nicolasgonzalez.frontend@gmail.com>
2021-03-10 12:16:47 -0300

    my first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,62 @@
+import { Component } from '@angular/core';
+import {Product} from './product.model'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'platzi-store';
+  items = ['nicolas', 'julian', 'samuel'];
+  products: Product[] = [
+    {
+      id: '1',
+      image: 'assets/mockup/camiseta.webp',
+      title: 'Camiseta',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '2',
+      image: 'assets/mockup/hoodie.webp',
+      title: 'Hoodie',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '3',
+      image: 'assets/mockup/mug.webp',
+      title: 'Mug',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '4',
+      image: 'assets/mockup/pin.png',
+      title: 'Pin',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '5',
+      image: 'assets/mockup/stickers1.webp',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    },
+    {
+      id: '6',
+      image: 'assets/mockup/stickers2.png',
+      title: 'Stickers',
+      price: 80000,
+      description: 'bla bla bla bla bla'
+    }
+  ];
+  add() {
+    this.items.push(this.title)
+  };
+  delete(index: number) {
+    this.items.splice(index, 1);
+  };
+}
---
+++ src/app/components/product.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core'
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './porduct.component.html'
+})
+export class ProductComponent {
+
+ }
